d5ee4b008bc8877ddb89edf10fb0e1e283d1a6f5
Raoul Strackx <raoul.strackx@gmail.com>
2021-08-31 14:21:39 +0200

    Adding support for `fortanixvme` environment

--- src/unix/linux_like/linux/align.rs
+++ src/unix/linux_like/linux/align.rs
@@ -30,9 +30,10 @@ macro_rules! expand_align {
                 size: [u8; ::__SIZEOF_PTHREAD_MUTEXATTR_T],
             }
 
-            #[cfg_attr(any(target_env = "musl", target_env = "ohos", target_pointer_width = "32"),
+            #[cfg_attr(any(target_env = "fortanixvme", target_env = "musl", target_env = "ohos", target_pointer_width = "32"),
                        repr(align(4)))]
-            #[cfg_attr(all(not(target_env = "musl"),
+            #[cfg_attr(all(not(target_env = "fortanixvme"),
+                           not(target_env = "musl"),
                            not(target_env = "ohos"),
                            target_pointer_width = "64"),
                        repr(align(8)))]
@@ -66,16 +67,16 @@ macro_rules! expand_align {
         }
 
         s_no_extra_traits! {
-            #[cfg_attr(all(any(target_env = "musl", target_env = "ohos"),
+            #[cfg_attr(all(any(target_env = "fortanixvme", target_env = "musl", target_env = "ohos"),
                            target_pointer_width = "32"),
                        repr(align(4)))]
-            #[cfg_attr(all(any(target_env = "musl", target_env = "ohos"),
+            #[cfg_attr(all(any(target_env = "fortanixvme", target_env = "musl", target_env = "ohos"),
                            target_pointer_width = "64"),
                        repr(align(8)))]
-            #[cfg_attr(all(not(any(target_env = "musl", target_env = "ohos")),
+            #[cfg_attr(all(not(any(target_env = "fortanixvme", target_env = "musl", target_env = "ohos")),
                            target_arch = "x86"),
                        repr(align(4)))]
-            #[cfg_attr(all(not(any(target_env = "musl", target_env = "ohos")),
+            #[cfg_attr(all(not(any(target_env = "fortanixvme", target_env = "musl", target_env = "ohos")),
                            not(target_arch = "x86")),
                        repr(align(8)))]
             pub struct pthread_cond_t {
